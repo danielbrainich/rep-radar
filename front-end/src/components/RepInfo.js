a4a50811a2a5448fd238eb65b2540370ab513ac4
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-02-09 08:19:00 -0800

    added createdby to the main page

--- front-end/src/components/RepInfo.js
+++ front-end/src/components/RepInfo.js
@@ -43,15 +43,20 @@ function RepInfo() {
     return (
         <>
             {!isFormSubmitted && !isLoading && (
-                <div className="container vh-100 d-flex align-items-center justify-content-center">
-                    <div className="row justify-content-center">
-                        <div className="col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5">
-                            <div className="content-container">
-                                <h1 className="text-center">RepRadar</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="container vh-100 d-flex flex-column justify-content-between">
+                    <div className="my-auto">
+                        <div className="row justify-content-center">
+                            <div className="col-11 col-sm-10 col-md-8 col-lg-6 col-xl-5">
+                                <div className="content-container">
+                                    <h1 className="text-center">RepRadar</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div>
+                                <AddressForm onSubmit={handleFormSubmit} />
                             </div>
-                            <AddressForm onSubmit={handleFormSubmit} />
                         </div>
+                    </div>
+                    <div className="text-center mb-4">
+                        Created by <a href="https://www.danielbrainich.com" target="_blank" rel="noopener norefferer">danielbrainich</a>
                     </div>
                 </div>
             )}
